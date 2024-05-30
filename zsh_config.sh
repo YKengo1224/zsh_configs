f1b5579ec6f7da54175bbc6c94d3c5072e29f610
YKengo1224 <kengo_y_15@icloud.com>
2024-05-30 15:33:20 +0900

    renual start

---
+++ zsh_config.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/zsh
+
+#criate backup
+cp ~/.zshrc  ~/.zshrc_old
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm ~/.zshrc
+cp ./zshrc_home ~/.zshrc
+mv ~/.oh-my-zsh ./
+#powerlevel10k
+cp -r ./.fonts/ ~/
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/zsh_configs/.oh-my-zsh/custom/themes/powerlevel10k
+
+#zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/zsh_configs/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+#zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/zsh_configs/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+#zsh-abbr
+git clone https://github.com/olets/zsh-abbr --single-branch --branch main --depth 1 $HOME/zsh_configs/.oh-my-zsh/custom/plugins/zsh-abbr
+#z
+git clone git@github.com:rupa/z.git $HOME/zsh_configs/.oh-my-zsh/custom/plugins/z
+
+
+#
+source ~/.zshrc 
